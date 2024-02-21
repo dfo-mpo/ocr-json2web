3392fbf7240943ced9e06b5325bc40ac7871a841
zhipengwuDFO <157764067+zhipengwuDFO@users.noreply.github.com>
2024-02-21 09:41:43 -0800

    added comments

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import FileNameReader from "./components/homePage/FileNameReader";
 import LogoHeader from "./components/LogoHeader";
 
 export default function Home() {
+  //this is the home page
   return (
     <div className={styles.container}>
       <LogoHeader />
