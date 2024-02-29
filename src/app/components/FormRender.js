291e7524ee57f71608955205d476c3dea8f66ba3
zhipengwuDFO <zhipeng.wu@dfo-mpo.gc.ca>
2024-02-29 15:33:17 -0800

    formSetting

--- src/app/components/FormRender.js
+++ src/app/components/FormRender.js
@@ -4,10 +4,16 @@ import Form from "./Form";
 import { useState, useEffect, use } from "react";
 import styles from "./FormRender.module.css";
 //TODO: remove import formSetting from "../formSetting.json"
-import formSetting from "../formSetting.json"
+// import formSetting from "../formSetting.json"
 //TODO: add formSetting as a prop
-const FormRender = ({ items, folderName, fileName, reFetch, reFetchJson }) => {
- 
+const FormRender = ({
+  items,
+  folderName,
+  fileName,
+  reFetch,
+  reFetchJson,
+  formSetting,
+}) => {
   const [isForm, setIsForm] = useState(true);
   const saveChange = () => {
     reFetch();
@@ -16,8 +22,7 @@ const FormRender = ({ items, folderName, fileName, reFetch, reFetchJson }) => {
   const cancelChange = () => {
     reFetchJson();
   };
- 
-  
+
   return (
     <>
       <button
@@ -26,14 +31,18 @@ const FormRender = ({ items, folderName, fileName, reFetch, reFetchJson }) => {
       >
         Switch
       </button>
-      
 
       {!isForm && (
         <SingleForm folderName={folderName} fileName={fileName} items={items} />
       )}
       {isForm && (
-        <Form folderName={folderName} formSetting = {formSetting} fileName={fileName} items={items} saveChange= {saveChange}
-        cancelChange={cancelChange}
+        <Form
+          folderName={folderName}
+          formSetting={formSetting}
+          fileName={fileName}
+          items={items}
+          saveChange={saveChange}
+          cancelChange={cancelChange}
         />
       )}
     </>
