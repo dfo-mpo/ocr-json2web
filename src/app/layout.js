512c030f4664386c055fe5bbd639330b1bb5ea34
zhipengwuDFO <zhipeng.wu@dfo-mpo.gc.ca>
2024-04-05 13:45:55 -0700

    fixed ORC to OCR

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ORC Project",
+  title: "OCR Project",
   description: "OCR Project",
 };
 
